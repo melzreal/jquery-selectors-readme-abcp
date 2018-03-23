54791f9a6fe9d9214e79f071a0f88666c000a817
Melissa Real <melissa.real@hotmail.com>
2018-03-23 13:52:41 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -20,5 +20,5 @@ return $('.pics');
 function firstListItem(){
   //return the first list item in the ul with the ID pic-list.
   
-  return $('pic-list li:first-child');
+  return $('pic-list ul:first-child');
 }
