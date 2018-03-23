98c364f9b8c5059b327cf8a1bbfef88f9b523e19
Melissa Real <melissa.real@hotmail.com>
2018-03-23 13:48:18 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -14,7 +14,7 @@ return $('#baby-ninja');
 }
 
 function divSelector(){
-return $('.pics div:first-child second-child')  
+return $('div .pics:first-child second-child')  
 }
 
 function firstListItem(){
